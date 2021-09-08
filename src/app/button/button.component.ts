0b69fd8e95a2834674947dfbe5de88d18bfe9dd8
ameya1301999 <ameyarathi1301999@gmail.com>
2021-09-08 09:51:11 +0530

    likedislike

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -2,12 +2,47 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  template: '<html><head><title> LikeDislike Button</title></head><body> <button class="{{b1Class}}" (click) ="goLike()">{{likeLable}} {{likeCounter}}</button> <button class={{b2Class}} (click) ="goDisLike()">{{dislikeLable}} {{dislikeCounter}}</button> </body> </html>',
+  styles: [
+    `
+      .Liked {
+        background-color: green;
+        color: white;
+        padding: 15px 32 px;
+        font-size: 16px;
+      }
+
+      .disLiked {
+        background-color: red;
+        color: white;
+        padding: 15px 32 px;
+        font-size: 16px;
+      }    
+    `
+  ]
+  //templateUrl: './button.component.html',
+  //styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
 
-  
+
+  b1Class: string = "Liked";
+  b2Class: string = "disLiked";
+
+  likeCounter: number = 100;
+  dislikeCounter: number = 25;
+
+  likeLable: string = 'Like'; 
+  dislikeLable: string = 'DisLike';
+
+  goLike() {
+    console.log('Like is Clicked');
+  }
+
+  goDisLike() {
+    console.log('Disliked is Clicked');
+  }
+
   constructor() { }
 
   ngOnInit(): void {
