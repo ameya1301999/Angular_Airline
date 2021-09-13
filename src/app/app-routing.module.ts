71ed214087ff9057c27d8c3572624be45919199d
ameya1301999 <ameyarathi1301999@gmail.com>
2021-09-13 09:46:16 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,29 +5,47 @@ import { HometComponent } from './homet/homet.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 import { ShowFlightsComponent } from './show-flights/show-flights.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
+// const routes: Routes = [
+//   {path: '**',component: PageNotFoundComponent}, 
+//   {path: '',redirectTo:'home',pathMatch:'full'}, 
+//   {path: 'home',component: HometComponent, 
+//       children: [
+//           {path: 'showFlights',component:ShowFlightsComponent},
+//           {path: 'register',component:RegistrationComponent},
+//           {path: 'login',component:LoginComponent,
+//             children: [
+//               {path: 'dashboard', component:UserDashboardComponent},
+//               {path: 'forgot',component:ForgotpasswordComponent,
+//                 children: [
+//                   {path: 'reset',component:ForgotpasswordComponent},
+//                 ]
+//               },
+//             ]
+//           },
+//       ]
+//   },
+// ];
+
 const routes: Routes = [
-  {path: '**',component: PageNotFoundComponent}, 
-  {path: '',redirectTo:'home',pathMatch:'full'}, 
-  {path: 'home',component: HometComponent, 
-      children: [
-          {path: 'showFlights',component:ShowFlightsComponent},
-          {path: 'register',component:RegistrationComponent},
-          {path: 'login',component:LoginComponent,
-            children: [
-              {path: 'dashboard', component:UserDashboardComponent},
-              {path: 'forgot',component:ForgotpasswordComponent,
-                children: [
-                  {path: 'reset',component:ForgotpasswordComponent},
-                ]
-              },
-            ]
-          },
-      ]
+  { path: 'homet', component: HometComponent},
+  { path: '', redirectTo:'home', pathMatch:'full'},
+  { path : 'register', component: RegistrationComponent },
+  { path : 'showflights', component: ShowFlightsComponent },
+  { path : 'login', component: LoginComponent,
+    children: [
+      { path : 'forgot', component: ForgotpasswordComponent},
+      { path : 'reset', component: ResetpasswordComponent}
+    ]
   },
-];
+  { path: 'dashboard', component: UserDashboardComponent},
+  //{ path: 'forgotpassword', component: ForgotpasswordComponent},
+  //{ path:'resetpassword', component: ResetpasswordComponent},
+  { path: '**', component: PageNotFoundComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/homet/homet.component.ts
+++ src/app/homet/homet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homet',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HometComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    this.route.navigate(['showflights']);
   }
 
 }
